4529649dff10fdb082b2d47bf33cce49f04367a8
nandariskan <silvercodee@gmail.com>
2020-08-12 22:34:51 +0700

    config base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:2121/hanura2020_42/';
+$config['base_url'] = 'http://localhost:2121/hanura';
 
 /*
 |--------------------------------------------------------------------------
