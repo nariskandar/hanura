b4c973f74c4a1efe2a502f983eba54ea8433d870
nandariskan <silvercodee@gmail.com>
2020-08-13 14:22:16 +0700

    hitung

--- application/views/layout/menubar.php
+++ application/views/layout/menubar.php
@@ -2,7 +2,6 @@
     <div class="page-sidebar-inner">
         <ul class="menu accordion-menu">
             <li class="nav-heading"><span>Navigation</span></li>
-            <!-- <li><a href="<?= base_url(); ?>"><span class="menu-icon fa fa-home"></span><p>Beranda</p></a></li> -->
             <li><a href="<?= base_url(); ?>rekom"><span class="menu-icon fa fa-th-list"></span><p>Data Rekom</p></a></li>
             <li><a href="<?= base_url(); ?>chart"><span class="menu-icon fa fa-pie-chart"></span><p>Chart</p></a></li>
             <li class="droplink"><a href="#"><span class="menu-icon fa fa-user"></span><p>Pengguna Manajemen</p><span class="arrow"></span></a>
