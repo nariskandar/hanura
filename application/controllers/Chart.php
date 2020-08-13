f3ce961af64ff48506f72c24e121e2196c6ff0c5
nandariskan <silvercodee@gmail.com>
2020-08-13 11:19:34 +0700

    judul chart

--- application/controllers/Chart.php
+++ application/controllers/Chart.php
@@ -12,14 +12,14 @@ class Chart extends CI_Controller
 
     public function index()
     {
-        // $data['judul'] = 'Halaman Data';
+        $data['judul'] = 'Halaman Chart';
         // $data['rekomendasi'] = $this->Rekom_model->getAllDataRekom();
         // $data['jmlh_hanura'] = $this->Rekom_model->getJumlahHanura();
 
-        $this->load->view('layout/header');
-        $this->load->view('layout/navbar');
-        $this->load->view('layout/menubar');
-        $this->load->view('chart/index');
-        $this->load->view('layout/footer');
+        $this->load->view('layout/header', $data);
+        $this->load->view('layout/navbar', $data);
+        $this->load->view('layout/menubar', $data);
+        $this->load->view('chart/index', $data);
+        $this->load->view('layout/footer', $data);
     }
 }
